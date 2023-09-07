4a6a25a5fe3b0e6c86597b44ac04ee7e307a6d3b
Muath <muath.khalifa@yahoo.com>
2023-09-08 00:45:45 +0100

    deploying

--- server/index.js
+++ server/index.js
@@ -32,7 +32,7 @@ console.log("location: ", location);
 app.use(express.static(path.join(__dirname, "../client/dist")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  res.sendFile(path.join(__dirname, "client/dist", "index.html"));
 
   // res.sendFile(path.join(__dirname, "client/build", "index.html"));
 });
